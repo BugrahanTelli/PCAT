7f24e08b6cd5485bb8da73c0bb7207e7534d9714
BugrahanTelli <mbugrahan.telli@gmail.com>
2024-10-05 15:20:21 +0300

    EJS Done

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require("express")
+const ejs = require("ejs")
 const path = require("path")
 
+
 const app = express()
+
+//Template Engine
+app.set("view engine", "ejs")
 
 
 //Middlewares (req ile res arasında yaptığımız işlemler)
 app.use(express.static('public'))
 
-
+//Routes
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname,".temp/index.html"))
+    res.render("index")
+})
+app.get("/about", (req, res) => {
+    res.render("about")
+})
+app.get("/add", (req, res) => {
+    res.render("add")
 })
 
 const port = 3000
