80289683ed3775f7ff6701b61def140c805aacff
BugrahanTelli <mbugrahan.telli@gmail.com>
2024-10-07 18:28:58 +0300

    Model_and_dynamic_content

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const ejs = require("ejs")
 const path = require("path")
+const Photo = require("./models/Photo")
 
 
 const app = express()
+
+//connect db
+mongoose.connect("mongodb://localhost/pcat-test-db")
 
 //Template Engine
 app.set("view engine", "ejs")
@@ -12,9 +17,17 @@ app.set("view engine", "ejs")
 //Middlewares (req ile res arasında yaptığımız işlemler)
 app.use(express.static('public'))
 
+app.use(express.urlencoded({ extended : true }))
+
+app.use(express.json())
+
+
 //Routes
-app.get("/", (req, res) => {
-    res.render("index")
+app.get("/", async (req, res) => {
+    const photos = await Photo.find({})
+    res.render("index",{
+        photos
+    })
 })
 app.get("/about", (req, res) => {
     res.render("about")
@@ -22,6 +35,15 @@ app.get("/about", (req, res) => {
 app.get("/add", (req, res) => {
     res.render("add")
 })
+app.post("/photos", async (req, res) => {
+    await Photo.create(req.body)
+    res.redirect("/")
+})
+
+
+
+
+
 
 const port = 3000
 
